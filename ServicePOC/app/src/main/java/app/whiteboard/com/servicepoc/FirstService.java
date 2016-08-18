ab6e98348467e0957f85019d9a90b1e62e69526f
Mohit Verma <mohitverma2323@gmail.com>
2016-08-18 18:23:19 +0530

    service runs in BG till OS closes it

--- ServicePOC/app/src/main/java/app/whiteboard/com/servicepoc/FirstService.java
+++ ServicePOC/app/src/main/java/app/whiteboard/com/servicepoc/FirstService.java
@@ -2,13 +2,24 @@ package app.whiteboard.com.servicepoc;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class FirstService extends Service {
 
     private static String TAG = "SampleServiceApp";
+    private File targetDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+    File outFile;
+    FileWriter fileWriter;
+    BufferedWriter writer;
+
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -21,21 +32,42 @@ public class FirstService extends Service {
         // TODO Auto-generated method stub
         super.onStart(intent, startId);
         Log.d(TAG, "FirstService started");
-        while(true){
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            Log.d(TAG,"Service is running");
+        outFile = new File(targetDir, "servicePOC.txt");
+        try {
+            fileWriter =new FileWriter(outFile, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        writer = new BufferedWriter(fileWriter);
+
+        save("Hey this is a service POC ");
+    }
+
+    public void save(String textContent){
+        try{
+           writer.write(textContent);
+        }
+        catch (Exception ex){
+            Log.e("Exception occurred!!!", ex.getMessage());
+        }
+        simulateDelay();
+
+    }
+    private void simulateDelay(){
+        try{
+            //Thread.sleep(3000);
+            save("this again");
+        }catch(Exception ex)
+        {
+            // Only likely exception is an InterruptedException which we can ignore
         }
     }
 
-    @Override
+   @Override
     public void onDestroy() {
         // TODO Auto-generated method stub
         super.onDestroy();
-        Log.d(TAG, "FirstService destroyed");
+        Log.d(TAG, "ServicePOC service was terminated");
     }
 
 }
--- ServicePOC/app/src/main/java/app/whiteboard/com/servicepoc/MainActivity.java
+++ ServicePOC/app/src/main/java/app/whiteboard/com/servicepoc/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, FirstService.class));
-    }
+}
 
 
     @Override
